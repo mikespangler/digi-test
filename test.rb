c68a1a7b56690a7c4cde8f2fdb5f429392286edb
Mike Spangler <spangler.mike@gmail.com>
2014-04-19 13:51:46 -0400

    first commit

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'aws-sdk'
+require 'pry'
+require File.expand_path("./", __FILE__)
+
+get '/' do
+  # cf = AWS::CloudFront.new(
+  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+  # binding.pry
+  # bucket = s3.buckets['beauty-test']
+  # video = bucket.objects['oceans.mp4']
+  #video = "https://s3-us-west-2.amazonaws.com/beauty-test/oceans.mp4"
+  erb :index#, :locals => { :video => video }
+end
