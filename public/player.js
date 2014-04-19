744bff50af0aefc9698934a916cb20737c53f2d7
Mike Spangler <spangler.mike@gmail.com>
2014-04-19 15:17:22 -0400

    video opens fullscreen

---
+++ public/player.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  var player = videojs('beauty_video');
+  player.requestFullScreen();
+  var aspectRatio = 9/16; // Make up an aspect ratio
+
+  function resizeVideoJS(player, aspectRatio){
+    var width = player.a.parentElement.offsetWidth;
+    // Set width to fill parent element, Set height
+    player.width(width).height( width * aspectRatio );
+  }
+    resizeVideoJS(player, aspectRatio); // Initialize the function
+    window.onresize = resizeVideoJS; // Call the function on resize
+});
