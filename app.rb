3412cb75c3152ceb9b56a1506e3c3057e34110f4
mikespangler <spangler.mike@gmail.com>
2014-06-24 09:17:12 -0400

    update app

--- app.rb
+++ app.rb
@@ -1,12 +1,5 @@
-# def cloudfront
-#   AWS::CloudFront.new(
-#     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-#     ).client
-# end
-
 get '/:filename' do
   filename = params[:filename]
-  video_url = "d1c6ne8ajn3h3m.cloudfront.net/#{filename}"
+  video_url = "http://d1c6ne8ajn3h3m.cloudfront.net/#{filename}.mp4"
   erb :player, locals: { video_url: video_url }
 end
