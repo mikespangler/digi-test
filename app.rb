98cdb4d3c8901db59f4a103c12c5197b14d88a1e
mikespangler <spangler.mike@gmail.com>
2014-06-23 23:00:10 -0400

    update cloudfront

--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
-get '/' do
-  # cf = AWS::CloudFront.new(
-  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
-  # binding.pry
-  # bucket = s3.buckets['beauty-test']
-  # video = bucket.objects['oceans.mp4']
-  #video = "https://s3-us-west-2.amazonaws.com/beauty-test/oceans.mp4"
-  erb :index#, :locals => { :video => video }
-end
+# def cloudfront
+#   AWS::CloudFront.new(
+#     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+#     ).client
+# end
 
-get '/message' do
-  erb :message
+get '/:filename' do
+  filename = params[:filename]
+  video_url = "d1c6ne8ajn3h3m.cloudfront.net/#{filename}"
+  erb :player, :locals => { :video_url => video_url }
 end
