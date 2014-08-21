a11ebdf6f9450a4833e90553bb8afa717b33d55b
mikespangler <spangler.mike@gmail.com>
2014-08-21 10:37:28 -0400

    hard code ocean video

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-get '/:filename' do
+get '/' do
   filename = params[:filename]
   video_url = "https://s3-us-west-2.amazonaws.com/beauty-test/oceans.mp4"
   erb :player, locals: { video_url: video_url }
